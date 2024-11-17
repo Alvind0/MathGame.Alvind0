98f394cefee8347b893b2318853ea5b346056ee6
Alvind0 <170922866+Alvind0@users.noreply.github.com>
2024-11-17 13:58:32 +0900

    Add c# console app files

---
+++ MathGame.Alvind0/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * This is a math game containing the 4 basic operations
+ * (addition, subtration, multiplication, division) with a
+ * main menu to choose operations and the ability to record 
+ * previous games untill the application ends.
+ */
